fd56d47fd5d048cbcaedb671c4e1dba4640ef0da
Pedro Henrique <phmachado555@gmail.com>
2023-09-26 14:43:13 -0300

    update and delete cliente-service

--- unifaa-challenge-react/src/api/cliente-service.js
+++ unifaa-challenge-react/src/api/cliente-service.js
@@ -17,3 +17,26 @@ function adicionar(cliente){
         .catch(erro => reject(erro))
     });
 }
+
+function atualizar(cliente){
+    return new Promise((resolve, reject) => {
+        service.put(`/clientes/${cliente.id}`, cliente)
+        .then(response => resolve(response))
+        .catch(erro => reject(erro))
+    });
+}
+
+function excluir(id){
+    return new Promise((resolve, reject) => {
+        service.delete(`/clientes/${id}`)
+        .then(response => resolve(response))
+        .catch(erro => reject(erro))
+    });
+}
+
+export default {
+    obter,
+    adicionar,
+    atualizar,
+    excluir
+}
